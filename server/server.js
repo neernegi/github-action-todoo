b5ef4a4108c1e5a357069a8bd0720ac9fc5c5d57
Neeraj <neerajnegi3443@gmail.com>
2025-04-30 23:14:15 +0530

    last

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import todoRoutes from "./routes/todoRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
